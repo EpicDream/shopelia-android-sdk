7bae521de64fd3163a8fe591d64fba8a600ed6ff
Pierre Pollastri <pollastri.p@gmail.com>
2013-04-19 11:17:57 +0200

    Add CharSequenceUtils easing CharSequence Manipulation

---
+++ src/com/shopelia/utils/CharSequenceUtils.java
@@ -0,0 +1,27 @@
+package com.shopelia.utils;
+
+/**
+ * Utility class easing {@link CharSequence} manipulation and functionality
+ * extensions.
+ * 
+ * @author Pierre Pollastri
+ */
+public final class CharSequenceUtils {
+
+    private CharSequenceUtils() {
+
+    }
+
+    /**
+     * This method checks if the given {@link CharSequence} is not null nor
+     * empty.
+     * 
+     * @param string
+     * @return True if the {@link CharSequence} is null or empty, false
+     *         otherwise
+     */
+    public static boolean isEmpty(CharSequence string) {
+        return string != null && string.length() > 0;
+    }
+
+}
