e606eee20345d4fb2cd0464b4510ac954e5712cf
Pierre Pollastri <pollastri.p@gmail.com>
2013-05-06 12:17:47 +0200

    (BugFix) Next focus on GONE views

--- src/com/shopelia/android/CreateAddressActivity.java
+++ src/com/shopelia/android/CreateAddressActivity.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AutoCompleteTextView;
@@ -117,6 +121,19 @@ public class CreateAddressActivity extends HostActivity {
 
             }
 
+        });
+
+        mAddressExtrasField.setOnKeyListener(new OnKeyListener() {
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && mCityField.getVisibility() != View.VISIBLE) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mAddressExtrasField.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
         });
 
         View headerFrame = findViewById(R.id.header_frame);
