eac29a219d01ede7e08be4f00a495ee19252e65a
Pierre Pollastri <pollastri.p@gmail.com>
2013-05-06 10:56:27 +0200

    Add save state for WaitingView

--- src/com/shopelia/android/widget/WaitingView.java
+++ src/com/shopelia/android/widget/WaitingView.java
@@ -10,6 +10,8 @@ import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Handler;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -28,27 +30,31 @@ public class WaitingView extends View {
     private static final long FRAMERATE = 40;
     private static final long FREQUENCY = 1000L / FRAMERATE;
 
-    private long mExpectedTime = 30000L;
+    private static int STATE_STARTED = 0;
+    private static int STATE_PAUSED = 1;
+    private static int STATE_STOPPED = 2;
 
-    private Interpolator mInterpolator;
+    private long mExpectedTime = 45000L;
+
+    private Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
 
     private Handler mHandler = new Handler();
 
     // Variables
     private long mCurrentTime = 0L;
+    private int mState = STATE_STOPPED;
 
     // Draw properties
     private float mProgressThickness = 40;
     private RectF mOval = new RectF();
     private Typeface mTypeface;
-    
+
     // Render objects
     private Paint mProgressPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mCenterCircleBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DEV_KERN_TEXT_FLAG | Paint.SUBPIXEL_TEXT_FLAG);
-    
-    
+
     public WaitingView(Context context) {
         this(context, null);
     }
@@ -61,7 +67,7 @@ public class WaitingView extends View {
         super(context, attrs, defStyle);
 
         init();
-        
+
         setProgressColor(0xFF2d9f35);
         setBackgroundColor(0xFFd2d2d2);
         setCenterCircleBackgroundColor(0xFFe5e5e5);
@@ -69,23 +75,33 @@ public class WaitingView extends View {
     }
 
     public void init() {
-    	if (!isInEditMode()) {
-    		mTypeface = FontableTextView.tryCreateTypefaceFromAsset(getContext().getAssets(), FontableTextView.ASAP_BOLD);
-    	} 
+        if (!isInEditMode()) {
+            mTypeface = FontableTextView.tryCreateTypefaceFromAsset(getContext().getAssets(), FontableTextView.ASAP_BOLD);
+        }
 
-    	if (mTypeface == null) {
-    		mTypeface = Typeface.DEFAULT_BOLD;
-    	}
+        if (mTypeface == null) {
+            mTypeface = Typeface.DEFAULT_BOLD;
+        }
     }
-    
+
+    public void restart() {
+        stop();
+        start();
+    }
+
     public void start() {
         mInterpolator = new AccelerateDecelerateInterpolator();
         mHandler.removeCallbacks(mAnimationRunnable);
         mHandler.post(mAnimationRunnable);
     }
 
+    public void pause() {
+        mHandler.removeCallbacks(mAnimationRunnable);
+    }
+
     public void stop() {
         mHandler.removeCallbacks(mAnimationRunnable);
+        mCurrentTime = 0;
     }
 
     public void setProgressColor(int color) {
@@ -100,7 +116,7 @@ public class WaitingView extends View {
         invalidate();
     }
 
-    @SuppressLint("NewApi") 
+    @SuppressLint("NewApi")
     public void setCenterCircleBackgroundColor(int color) {
         mCenterCircleBackgroundPaint.setColor(color);
         mCenterCircleBackgroundPaint.setStyle(Style.FILL);
@@ -112,11 +128,35 @@ public class WaitingView extends View {
     }
 
     public void setTextColor(int color) {
-    	mTextPaint.setColor(color);
-    	mTextPaint.setTypeface(mTypeface);
-    	mTextPaint.setTextAlign(Align.CENTER);
+        mTextPaint.setColor(color);
+        mTextPaint.setTypeface(mTypeface);
+        mTextPaint.setTextAlign(Align.CENTER);
     }
-    
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable parcelable = super.onSaveInstanceState();
+        SavedState savedState = new SavedState(parcelable);
+        savedState.mCurrentTime = mCurrentTime;
+        savedState.mExpectedTime = mExpectedTime;
+        savedState.mState = mState;
+        return savedState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        super.onRestoreInstanceState(state);
+        if (state instanceof SavedState) {
+            SavedState savedState = (SavedState) state;
+            mCurrentTime = savedState.mCurrentTime;
+            mExpectedTime = savedState.mExpectedTime;
+            mState = savedState.mState;
+            if (mState == STATE_STARTED) {
+                start();
+            }
+        }
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -127,22 +167,22 @@ public class WaitingView extends View {
         mOval.set(x - radius, y - radius, x + radius, y + radius);
         float progress = 0.f;
         if (!isInEditMode()) {
-        	progress = mCurrentTime < mExpectedTime ? mInterpolator.getInterpolation((float)mCurrentTime / (float)mExpectedTime) : 1.f;
+            progress = mCurrentTime < mExpectedTime ? mInterpolator.getInterpolation((float) mCurrentTime / (float) mExpectedTime) : 1.f;
         } else {
-        	progress = 0.45f;
+            progress = 0.45f;
         }
-        
+
         int angle = (int) (MAX_ANGLE * progress);
-      
+
         // Draw background "grey" circle
         canvas.drawCircle(x, y, radius, mBackgroundPaint);
-        
-        // Draw progress "green" indicator 
+
+        // Draw progress "green" indicator
         canvas.drawArc(mOval, -90, angle, true, mProgressPaint);
-        
+
         // Draw center circle
         canvas.drawCircle(x, y, radius - mProgressThickness, mCenterCircleBackgroundPaint);
-      
+
         String text = String.valueOf((int) (progress * 100));
         float textSize = radius * 0.40f;
         mTextPaint.setTextSize(textSize);
@@ -150,7 +190,7 @@ public class WaitingView extends View {
         float textY = y - (mTextPaint.ascent() + mTextPaint.descent()) / 2;
         text += "%";
         canvas.drawText(text, textX, textY, mTextPaint);
-        
+
     }
 
     private Runnable mAnimationRunnable = new Runnable() {
@@ -161,9 +201,36 @@ public class WaitingView extends View {
             invalidate();
             mHandler.postDelayed(this, FREQUENCY);
             if (mCurrentTime > mExpectedTime + FREQUENCY) {
-            	stop();
+                stop();
             }
         }
     };
+
+    private static class SavedState extends View.BaseSavedState {
+
+        private long mCurrentTime;
+        private long mExpectedTime;
+        private int mState;
+
+        public SavedState(Parcel source) {
+            super(source);
+            mCurrentTime = source.readLong();
+            mExpectedTime = source.readLong();
+            mState = source.readInt();
+        }
+
+        public SavedState(Parcelable parcelable) {
+            super(parcelable);
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeLong(mCurrentTime);
+            dest.writeLong(mExpectedTime);
+            dest.writeInt(mState);
+        }
+
+    }
 
 }
