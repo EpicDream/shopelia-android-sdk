a9e264e67c6c889a3828622e9bf794b6d589821c
Pierre Pollastri <pollastri.p@gmail.com>
2013-05-13 13:55:24 +0200

    PincodeActivity

--- src/com/shopelia/android/PincodeActivity.java
+++ src/com/shopelia/android/PincodeActivity.java
@@ -1,14 +1,27 @@
 package com.shopelia.android;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 
-public class PincodeActivity extends Activity {
+import com.shopelia.android.app.HostActivity;
+
+public class PincodeActivity extends HostActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHostContentView(R.layout.shopelia_process_order_activity);
 
+        if (savedInstanceState == null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.fragment_container, new PincodeFragment());
+            ft.commit();
+        }
+    }
+
+    @Override
+    protected boolean isPartOfOrderWorkFlow() {
+        return false;
     }
 
 }
--- src/com/shopelia/android/PincodeFragment.java
+++ src/com/shopelia/android/PincodeFragment.java
@@ -1,11 +1,9 @@
 package com.shopelia.android;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 
 import com.shopelia.android.app.ShopeliaFragment;
 
@@ -14,14 +12,11 @@ public class PincodeFragment extends ShopeliaFragment<Void> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.shopelia_pincode_fragment, container, false);
     }
 
     @Override
