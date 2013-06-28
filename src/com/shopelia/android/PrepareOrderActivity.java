f3caad6e0d2e84a147f9c326ef501eece7d33992
Pierre Pollastri <pollastri.p@gmail.com>
2013-06-28 12:02:25 +0200

    Fix email bug

--- src/com/shopelia/android/PrepareOrderActivity.java
+++ src/com/shopelia/android/PrepareOrderActivity.java
@@ -201,12 +201,14 @@ public class PrepareOrderActivity extends ShopeliaActivity implements OnSignUpLi
         }
         super.onActivityResult(requestCode, resultCode, data);
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-        if (resultCode == ShopeliaActivity.RESULT_LOGOUT && fragment == mSignUpFragment) {
-            switchFragments(mSignInFragment, mSignUpFragment);
-        } else if (fragment == null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.fragment_container, mSignUpFragment);
-            ft.commit();
+        if (!UserManager.get(this).isLogged()) {
+            if (resultCode == ShopeliaActivity.RESULT_LOGOUT && fragment == mSignUpFragment) {
+                switchFragments(mSignInFragment, mSignUpFragment);
+            } else if (fragment == null) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.fragment_container, mSignUpFragment);
+                ft.commit();
+            }
         }
         switch (requestCode) {
             case REQUEST_CHECKOUT:
