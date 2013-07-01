8a264843f31d943c69a27b8eef644894350f0840
Pierre Pollastri <pollastri.p@gmail.com>
2013-07-01 17:47:50 +0200

    LocaleUtils can be used to retrieve countries

--- src/com/shopelia/android/utils/LocaleUtils.java
+++ src/com/shopelia/android/utils/LocaleUtils.java
@@ -1,5 +1,7 @@
 package com.shopelia.android.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public final class LocaleUtils {
@@ -22,6 +24,17 @@ public final class LocaleUtils {
 
     public static String getCountryDisplayName(String iso) {
         return new Locale("", iso).getDisplayCountry();
+    }
+
+    public static List<String> getCountries() {
+        Locale[] locales = Locale.getAvailableLocales();
+        List<String> countries = new ArrayList<String>(locales.length);
+        for (Locale locale : locales) {
+            if (!countries.contains(locale.getDisplayCountry())) {
+                countries.add(locale.getDisplayCountry());
+            }
+        }
+        return countries;
     }
 
 }
