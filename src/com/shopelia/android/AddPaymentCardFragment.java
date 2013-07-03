b9f0801f9627e87f375a3ec3daac62d1e3ccb590
Pierre Pollastri <pollastri.p@gmail.com>
2013-07-03 16:53:56 +0200

    Cosmetic changes on AddPaymentCardActivity

--- src/com/shopelia/android/AddPaymentCardFragment.java
+++ src/com/shopelia/android/AddPaymentCardFragment.java
@@ -3,11 +3,24 @@ package com.shopelia.android;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.shopelia.android.app.ShopeliaFragment;
+import com.shopelia.android.model.Order;
+import com.shopelia.android.model.PaymentCard;
+import com.shopelia.android.widget.form.FormLinearLayout;
+import com.shopelia.android.widget.form.HeaderField;
+import com.shopelia.android.widget.form.SingleLinePaymentCardField;
 
 public class AddPaymentCardFragment extends ShopeliaFragment<Void> {
+
+    public interface OnPaymentCardAddedListener {
+        public void onPaymentCardAdded(PaymentCard card);
+    }
+
+    private FormLinearLayout mFormContainer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -17,6 +30,22 @@ public class AddPaymentCardFragment extends ShopeliaFragment<Void> {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mFormContainer = findViewById(R.id.form);
+        mFormContainer.findFieldById(R.id.header, HeaderField.class).displayLock();
+        mFormContainer.findFieldById(R.id.payment_card, SingleLinePaymentCardField.class).setJsonPath(Order.Api.PAYMENT_CARD);
+        mFormContainer.onCreate(savedInstanceState);
+
+        findViewById(R.id.validate).setOnClickListener(mOnValidateClickListener);
     }
+
+    private OnClickListener mOnValidateClickListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (mFormContainer.validate()) {
+                Toast.makeText(getActivity(), "Ça marche !", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
 }
