e5cd48da70e67f8a58a05b27412c344ef88b8269
Pierre Pollastri <pollastri.p@gmail.com>
2013-07-18 17:35:20 +0200

    Add methods to get metadata easily

--- src/com/shopelia/android/utils/ContextUtils.java
+++ src/com/shopelia/android/utils/ContextUtils.java
@@ -1,9 +1,15 @@
 package com.shopelia.android.utils;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.text.TextUtils;
 
 public final class ContextUtils {
+
+    private static final String APPLICATION_NAME = "shopelia-sdk-application-name";
 
     private ContextUtils() {
 
@@ -24,6 +30,51 @@ public final class ContextUtils {
             }
         }
         return true;
+    }
+
+    public static ApplicationInfo getApplicationInfo(Context context) {
+        if (context == null) {
+            return null;
+        }
+        Context app = context.getApplicationContext();
+        try {
+            return app.getPackageManager().getApplicationInfo(app.getPackageName(), PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    public static Bundle getMetadata(Context context) {
+        ApplicationInfo ai = getApplicationInfo(context);
+        if (ai != null) {
+            return ai.metaData;
+        }
+        return null;
+    }
+
+    public static Object getMetadata(Context context, String key) {
+        Bundle metadata = getMetadata(context);
+        if (metadata != null) {
+            return metadata.get(key);
+        }
+        return null;
+    }
+
+    public static String getMetadataString(Context context, String key, String fallback) {
+        String out = (String) getMetadata(context, key);
+        return out != null ? out : fallback;
+    }
+
+    public static String getApplicationName(Context context, String fallback) {
+        if (context == null) {
+            return null;
+        }
+        String appName = getMetadataString(context, APPLICATION_NAME, null);
+        if (TextUtils.isEmpty(appName)) {
+            int resId = context.getApplicationInfo().labelRes;
+            appName = context.getString(resId);
+        }
+        return TextUtils.isEmpty(appName) ? fallback : appName;
     }
 
 }
