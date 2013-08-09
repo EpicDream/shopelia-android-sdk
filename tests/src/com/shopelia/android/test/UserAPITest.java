795aaf4886a8ab84c6dcbcb9cc336231e723f478
Pierre Pollastri <pollastri.p@gmail.com>
2013-08-09 16:27:00 +0200

    Test sign out

--- tests/src/com/shopelia/android/test/UserAPITest.java
+++ tests/src/com/shopelia/android/test/UserAPITest.java
@@ -1,44 +1,23 @@
 package com.shopelia.android.test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.json.JSONObject;
-
 import android.test.InstrumentationTestCase;
 
+import com.shopelia.android.manager.UserManager;
 import com.shopelia.android.model.User;
-import com.shopelia.android.remote.api.ApiHandler.CallbackAdapter;
-import com.shopelia.android.remote.api.UserAPI;
 import com.shopelia.android.test.model.MockUser;
-import com.turbomanage.httpclient.HttpResponse;
 
 public class UserAPITest extends InstrumentationTestCase {
 
     public void testSignIn() {
-        final CountDownLatch barrier = new CountDownLatch(1);
-        final AtomicBoolean result = new AtomicBoolean(false);
-        new UserAPI(getInstrumentation().getTargetContext(), new CallbackAdapter() {
-
-            @Override
-            public void onSignIn(User user) {
-                result.set(true);
-                barrier.countDown();
-            }
-
-            @Override
-            public void onError(int step, HttpResponse httpResponse, JSONObject response, Exception e) {
-                barrier.countDown();
-            }
-
-        }).signIn(MockUser.get("me"));
-        try {
-            barrier.await(30, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assertTrue(result.get());
+        User me = MockUser.get("me");
+        TestUtils.signIn(getInstrumentation().getTargetContext(), me);
+        assertEquals("Should have email = " + me.email, UserManager.get(getInstrumentation().getTargetContext()).getUser().email, me.email);
+        assertTrue("Should be signed in", UserManager.get(getInstrumentation().getTargetContext()).isLogged());
     }
 
+    public void testSignOut() {
+        testSignIn();
+        TestUtils.signOut(getInstrumentation().getTargetContext());
+        assertFalse("Should not be logged ", UserManager.get(getInstrumentation().getTargetContext()).isLogged());
+    }
 }
