c829f17b5867d948714fcaebca5c3d2e8f0529e1
Pierre Pollastri <pollastri.p@gmail.com>
2013-08-12 12:49:20 +0200

    Add another address fixture

--- tests/src/com/shopelia/android/test/model/MockAddress.java
+++ tests/src/com/shopelia/android/test/model/MockAddress.java
@@ -19,4 +19,16 @@ public class MockAddress extends Address {
         zipcode = "75001";
 
     }
+
+    public void second() {
+        address = "12 rue du second test sur Android";
+        city = "Paris";
+        country = "FR";
+        extras = "3ème escalier à gauche";
+        firstname = "Test";
+        lastname = "Shopelia";
+        phone = "06058645869";
+        zipcode = "75001";
+
+    }
 }
