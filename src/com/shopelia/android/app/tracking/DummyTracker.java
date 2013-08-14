6bdc63bb8838502c7f8ee8715766ebf388d0a8e6
Pierre Pollastri <pollastri.p@gmail.com>
2013-08-14 15:32:35 +0200

    Add a flush delegate for Viking tracker

--- src/com/shopelia/android/app/tracking/DummyTracker.java
+++ src/com/shopelia/android/app/tracking/DummyTracker.java
@@ -1,13 +1,18 @@
 package com.shopelia.android.app.tracking;
 
+import java.util.LinkedList;
+
 import org.json.JSONObject;
 
 import android.content.Context;
 
+import com.shopelia.android.analytics.Analytics;
 import com.shopelia.android.app.ShopeliaTracker;
 import com.shopelia.android.model.User;
 
 public class DummyTracker extends ShopeliaTracker {
+
+    public static final LinkedList<Entry> LOGS = new LinkedList<DummyTracker.Entry>();
 
     @Override
     public void init(Context context) {
@@ -16,27 +21,27 @@ public class DummyTracker extends ShopeliaTracker {
 
     @Override
     public void track(String eventName) {
-
+        LOGS.add(new Entry(eventName));
     }
 
     @Override
     public void track(String eventName, JSONObject object) {
-
+        LOGS.add(new Entry(eventName, object.toString()));
     }
 
     @Override
     public void onDisplay(String activityName) {
-
+        LOGS.add(new Entry(Analytics.Events.Activities.DISPLAY, activityName));
     }
 
     @Override
     public void onFocusIn(String fieldName) {
-
+        LOGS.add(new Entry(Analytics.Events.UserInteractions.FOCUS_IN, fieldName));
     }
 
     @Override
     public void onValidate(String fieldName) {
-
+        LOGS.add(new Entry(Analytics.Events.UserInteractions.OK, fieldName));
     }
 
     @Override
@@ -46,11 +51,26 @@ public class DummyTracker extends ShopeliaTracker {
 
     @Override
     public void identify(User user) {
-
+        LOGS.add(new Entry(Analytics.Events.IDENTIFY, user.email));
     }
 
     @Override
     public void unidentify() {
+        LOGS.add(new Entry(Analytics.Events.UNIDENTIFY));
+    }
+
+    private static class Entry {
+        String eventName;
+        String extras;
+
+        public Entry(String eventName) {
+            this.eventName = eventName;
+        }
+
+        public Entry(String eventName, String extras) {
+            this.eventName = eventName;
+            this.extras = extras;
+        }
 
     }
 
