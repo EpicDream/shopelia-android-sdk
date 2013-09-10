b9d62601a07dcf850e759902ecb75737044141c2
Pierre Pollastri <pollastri.p@gmail.com>
2013-09-10 16:06:52 +0200

    ShopeliaTracker tested

--- tests/src/com/shopelia/android/app/tracking/ShopeliaTrackerTest.java
+++ tests/src/com/shopelia/android/app/tracking/ShopeliaTrackerTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import com.shopelia.android.app.tracking.ShopeliaTracker.FlushDelegate;
@@ -40,13 +41,69 @@ public class ShopeliaTrackerTest extends InstrumentationTestCase {
                 trackerName.value = t;
                 result.value = events.get(0).url;
                 barrier.countDown();
-                return true;
+                return false;
             }
         });
         tracker.onDisplayShopeliaButton(URL, "Test");
         barrier.await();
-        assertTrue("Should wait at least 2 seconds before flushing", onFlushTime.value - now >= 2 * TimeUnits.SECONDS);
+        assertTrue("Should wait at least 2 seconds before flushing instead of " + ((onFlushTime.value - now) / 1000), onFlushTime.value
+                - now >= 2 * TimeUnits.SECONDS);
         assertEquals("Test", trackerName.value);
         assertEquals(URL, result.value);
     }
+
+    @MediumTest
+    public void testMultipleEvents() throws InterruptedException {
+        final long now = System.currentTimeMillis();
+        final Mutable<Long> onFlushTime = new Mutable<Long>();
+        final Mutable<String> action = new Mutable<String>();
+        final Mutable<String> trackerName = new Mutable<String>();
+        final Mutable<Integer> result = new Mutable<Integer>();
+        final CountDownLatch barrier = new CountDownLatch(1);
+
+        tracker.setFlushDelegate(new FlushDelegate() {
+
+            int iteration = 0;
+
+            @Override
+            public boolean send(String uuid, String t, String a, ArrayList<ShopeliaEvent> events) {
+                assertTrue("Should be flushed once", iteration++ == 0);
+                onFlushTime.value = System.currentTimeMillis();
+                action.value = a;
+                trackerName.value = t;
+                result.value = events.size();
+                barrier.countDown();
+                return false;
+            }
+        });
+        trackDisplay(0, 20, "Test");
+        barrier.await();
+        assertTrue("Should wait at least 2 seconds before flushing instead of " + ((onFlushTime.value - now) / 1000), onFlushTime.value
+                - now >= 2 * TimeUnits.SECONDS);
+        assertEquals("Test", trackerName.value);
+        assertTrue(result.value.intValue() >= 19);
+    }
+
+    public void trackDisplay(int from, int to, String trackerName) {
+        for (; from < to; from++) {
+            tracker.onDisplayShopeliaButton(URL + from, trackerName);
+        }
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        final CountDownLatch barrier = new CountDownLatch(1);
+        tracker.setFlushDelegate(new FlushDelegate() {
+
+            @Override
+            public boolean send(String uuid, String tracker, String action, ArrayList<ShopeliaEvent> events) {
+                barrier.countDown();
+                return true;
+            }
+        });
+        tracker.flush();
+        barrier.await(3, java.util.concurrent.TimeUnit.SECONDS);
+        super.tearDown();
+    }
+
 }
