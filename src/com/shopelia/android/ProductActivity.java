e14c6ebafc998b3614aaeb9f4df863d305fbf412
Pierre Pollastri <pollastri.p@gmail.com>
2013-09-30 13:57:13 +0200

    Add a new activity for product view

---
+++ src/com/shopelia/android/ProductActivity.java
@@ -0,0 +1,14 @@
+package com.shopelia.android;
+
+import com.shopelia.android.app.ShopeliaActivity;
+
+public class ProductActivity extends ShopeliaActivity {
+
+    public static final String NAME = "Product";
+
+    @Override
+    public String getActivityName() {
+        return NAME;
+    }
+
+}
