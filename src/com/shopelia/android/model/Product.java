be16b182039ff3ec9b570656977bf8fe17988eb8
Pierre Pollastri <pollastri.p@gmail.com>
2013-10-18 18:26:06 +0200

    Minor changes

--- src/com/shopelia/android/model/Product.java
+++ src/com/shopelia/android/model/Product.java
@@ -16,248 +16,261 @@ import com.shopelia.android.utils.Tax;
 
 public class Product implements BaseModel<Product> {
 
-    public interface Api {
-        String URL = "url";
-        String MERCHANT = "merchant";
-        String VERSIONS = "versions";
-        String QUANTITY = "quantity";
-        String PRICE = "price";
-        String PRODUCT_VERSION_ID = "product_version_id";
+	public interface Api {
+		String URL = "url";
+		String PRODUCT_URL = "product_url";
+		String MERCHANT = "merchant";
+		String VERSIONS = "versions";
+		String QUANTITY = "quantity";
+		String PRICE = "price";
+		String PRODUCT_VERSION_ID = "product_version_id";
 
-    }
+	}
 
-    public static final String IDENTIFIER = Product.class.getName();
-    public static final float NO_PRICE = -1.f;
+	public static final String IDENTIFIER = Product.class.getName();
+	public static final float NO_PRICE = -1.f;
 
-    public String url;
+	public String url;
 
-    public Merchant merchant;
-    public Tax tax;
-    public Currency currency;
+	public Merchant merchant;
+	public Tax tax;
+	public Currency currency;
 
-    public final Versions versions;
-    public long mCurrentVersionKey;
+	public final Versions versions;
+	public long mCurrentVersionKey;
 
-    private int mQuantity = 1;
+	private int mQuantity = 1;
 
-    public Product(String url) {
-        this.url = url;
-        versions = new Versions();
-        ensureDefaultValues();
-    }
+	public Product(String url) {
+		this.url = url;
+		versions = new Versions();
+		ensureDefaultValues();
+	}
 
-    protected Product(Parcel source) {
-        url = source.readString();
-        mQuantity = source.readInt();
-        versions = new Versions();
-        Version version = ParcelUtils.readParcelable(source, Version.class.getClassLoader());
-        if (version != null) {
-            mCurrentVersionKey = version.getOptionHashcode();
-            versions.addVersion(version);
-        }
-        merchant = ParcelUtils.readParcelable(source, Merchant.class.getClassLoader());
-        tax = ParcelUtils.readParcelable(source, Tax.class.getClassLoader());
-        currency = ParcelUtils.readParcelable(source, Currency.class.getClassLoader());
-    }
+	protected Product(Parcel source) {
+		url = source.readString();
+		mQuantity = source.readInt();
+		versions = new Versions();
+		Version version = ParcelUtils.readParcelable(source,
+				Version.class.getClassLoader());
+		if (version != null) {
+			mCurrentVersionKey = version.getOptionHashcode();
+			versions.addVersion(version);
+		}
+		merchant = ParcelUtils.readParcelable(source,
+				Merchant.class.getClassLoader());
+		tax = ParcelUtils.readParcelable(source, Tax.class.getClassLoader());
+		currency = ParcelUtils.readParcelable(source,
+				Currency.class.getClassLoader());
+	}
 
-    public BigDecimal getTotalPrice() {
-        return getCurrentVersion().getTotalPrice(mQuantity);
-    }
+	public BigDecimal getTotalPrice() {
+		return getCurrentVersion().getTotalPrice(mQuantity);
+	}
 
-    public BigDecimal getProductPrice() {
-        return getCurrentVersion().getPrice(mQuantity);
-    }
+	public BigDecimal getProductPrice() {
+		return getCurrentVersion().getPrice(mQuantity);
+	}
 
-    public BigDecimal getExpectedTotalPrice() {
-        return getCurrentVersion().getExpectedTotalPrice(mQuantity);
-    }
+	public BigDecimal getExpectedTotalPrice() {
+		return getCurrentVersion().getExpectedTotalPrice(mQuantity);
+	}
 
-    public BigDecimal getExpectedCashfrontValue() {
-        return getCurrentVersion().getExpectedCashfrontValue(mQuantity);
-    }
+	public BigDecimal getExpectedCashfrontValue() {
+		return getCurrentVersion().getExpectedCashfrontValue(mQuantity);
+	}
 
-    public BigDecimal getSingleProductPrice() {
-        return getCurrentVersion().getPrice(1);
-    }
+	public BigDecimal getSingleProductPrice() {
+		return getCurrentVersion().getPrice(1);
+	}
 
-    public BigDecimal getStrikeoutPrice() {
-        return getCurrentVersion().getStrikeoutPrice();
-    }
+	public BigDecimal getStrikeoutPrice() {
+		return getCurrentVersion().getStrikeoutPrice();
+	}
 
-    public BigDecimal getShippingPrice() {
-        return getCurrentVersion().getShippingPrice();
-    }
+	public BigDecimal getShippingPrice() {
+		return getCurrentVersion().getShippingPrice();
+	}
 
-    public boolean hasCashfront() {
-        return getCurrentVersion().hasCashfront();
-    }
+	public boolean hasCashfront() {
+		return getCurrentVersion().hasCashfront();
+	}
 
-    @Override
-    public JSONObject toJson() throws JSONException {
-        JSONObject json = new JSONObject();
-        json.put(Api.PRODUCT_VERSION_ID, getCurrentVersion().getId());
-        json.put(Api.PRICE, getSingleProductPrice());
-        json.put(Api.QUANTITY, mQuantity);
-        return json;
-    }
+	@Override
+	public JSONObject toJson() throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put(Api.PRODUCT_VERSION_ID, getCurrentVersion().getId());
+		json.put(Api.PRICE, getSingleProductPrice());
+		json.put(Api.QUANTITY, mQuantity);
+		return json;
+	}
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+	@Override
+	public int describeContents() {
+		return 0;
+	}
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(url);
-        dest.writeInt(mQuantity);
-        ParcelUtils.writeParcelable(dest, getCurrentVersion(), flags);
-        ParcelUtils.writeParcelable(dest, merchant, flags);
-        ParcelUtils.writeParcelable(dest, tax, flags);
-        ParcelUtils.writeParcelable(dest, currency, flags);
-    }
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(url);
+		dest.writeInt(mQuantity);
+		ParcelUtils.writeParcelable(dest, getCurrentVersion(), flags);
+		ParcelUtils.writeParcelable(dest, merchant, flags);
+		ParcelUtils.writeParcelable(dest, tax, flags);
+		ParcelUtils.writeParcelable(dest, currency, flags);
+	}
 
-    public static final Parcelable.Creator<Product> CREATOR = new Creator<Product>() {
+	public static final Parcelable.Creator<Product> CREATOR = new Creator<Product>() {
 
-        @Override
-        public Product[] newArray(int size) {
-            return new Product[size];
-        }
+		@Override
+		public Product[] newArray(int size) {
+			return new Product[size];
+		}
 
-        @Override
-        public Product createFromParcel(Parcel source) {
-            return new Product(source);
-        }
-    };
+		@Override
+		public Product createFromParcel(Parcel source) {
+			return new Product(source);
+		}
+	};
 
-    public Product(JSONObject object, boolean hasVerions) throws JSONException {
-        url = object.optString(Api.URL);
-        if (object.has(Api.MERCHANT)) {
-            merchant = Merchant.inflate(object.getJSONObject(Api.MERCHANT));
-        }
-        if (hasVerions) {
-            versions = Versions.inflate(object.getJSONArray(Api.VERSIONS));
-        } else {
-            versions = new Versions();
-            versions.addVersion(Version.inflate(object));
-        }
-        mCurrentVersionKey = versions.getFirstKey();
-        ensureDefaultValues();
-    }
+	public Product(JSONObject object, boolean hasVerions) throws JSONException {
+		url = object.optString(Api.URL);
+		if (object.has(Api.PRODUCT_URL)) {
+			url = object.getString(Api.PRODUCT_URL);
+		}
+		if (object.has(Api.MERCHANT)) {
+			merchant = Merchant.inflate(object.getJSONObject(Api.MERCHANT));
+		}
+		if (hasVerions) {
+			versions = Versions.inflate(object.getJSONArray(Api.VERSIONS));
+		} else {
+			versions = new Versions();
+			versions.addVersion(Version.inflate(object));
+		}
+		mCurrentVersionKey = versions.getFirstKey();
+		ensureDefaultValues();
+	}
 
-    public static Product inflate(JSONObject object) throws JSONException {
-        return new Product(object, true);
-    }
+	public static Product inflate(JSONObject object) throws JSONException {
+		return new Product(object, true);
+	}
 
-    public static Product inflateSingleVersion(JSONObject object) throws JSONException {
-        return new Product(object, false);
-    }
+	public static Product inflateSingleVersion(JSONObject object)
+			throws JSONException {
+		return new Product(object, false);
+	}
 
-    public static ArrayList<Product> inflate(JSONArray a) throws JSONException {
-        final int size = a.length();
-        ArrayList<Product> products = new ArrayList<Product>(size);
-        for (int index = 0; index < size; index++) {
-            products.add(Product.inflate(a.getJSONObject(index)));
-        }
-        return products;
-    }
+	public static ArrayList<Product> inflate(JSONArray a) throws JSONException {
+		final int size = a.length();
+		ArrayList<Product> products = new ArrayList<Product>(size);
+		for (int index = 0; index < size; index++) {
+			products.add(Product.inflate(a.getJSONObject(index)));
+		}
+		return products;
+	}
 
-    protected void ensureDefaultValues() {
-        if (currency == null) {
-            currency = Currency.EUR;
-        }
+	protected void ensureDefaultValues() {
+		if (currency == null) {
+			currency = Currency.EUR;
+		}
 
-        if (tax == null) {
-            tax = Tax.ATI;
-        }
+		if (tax == null) {
+			tax = Tax.ATI;
+		}
 
-    }
+	}
 
-    @Override
-    @Deprecated
-    public void merge(Product cpy) {
+	@Override
+	@Deprecated
+	public void merge(Product cpy) {
 
-    }
+	}
 
-    public void setCurrentVersion(long key) {
-        mCurrentVersionKey = key;
-    }
+	public void setCurrentVersion(long key) {
+		mCurrentVersionKey = key;
+	}
 
-    public void setCurrentVersion(Option... options) {
-        setCurrentVersion(Option.hashCode(options));
-    }
+	public void setCurrentVersion(Option... options) {
+		setCurrentVersion(Option.hashCode(options));
+	}
 
-    public void setCurrentVersion(int lastOptionChanged, Option... options) {
-        Option[] opts = new Option[options.length];
-        System.arraycopy(options, 0, opts, 0, options.length);
-        if (_setCurrentVersion(0, lastOptionChanged, opts)) {
-            setCurrentVersion(opts);
-        }
-    }
+	public void setCurrentVersion(int lastOptionChanged, Option... options) {
+		Option[] opts = new Option[options.length];
+		System.arraycopy(options, 0, opts, 0, options.length);
+		if (_setCurrentVersion(0, lastOptionChanged, opts)) {
+			setCurrentVersion(opts);
+		}
+	}
 
-    private boolean _setCurrentVersion(int currentOption, int lastOptionChanged, Option[] options) {
-        // Check if the version is available
+	private boolean _setCurrentVersion(int currentOption,
+			int lastOptionChanged, Option[] options) {
+		// Check if the version is available
 
-        // Iterate through all option
-        // call _setCurrentVersion on another option
-        // If successfull stop
-        // else continue
+		// Iterate through all option
+		// call _setCurrentVersion on another option
+		// If successfull stop
+		// else continue
 
-        if (!versions.hasVersion(options)) {
-            Option before = options[currentOption];
-            Options opts = versions.getOptions(currentOption);
-            for (Option opt : opts) {
-                // Compute the next available options index
-                int next = getNextOptionIndexAvailable(currentOption + 1, lastOptionChanged);
-                if (next != -1 ? _setCurrentVersion(next, lastOptionChanged, options) : versions.hasVersion(options)) {
-                    return true;
-                }
-                options[currentOption] = opt;
-            }
-            options[currentOption] = before;
-            return false;
-        }
-        return true;
-    }
+		if (!versions.hasVersion(options)) {
+			Option before = options[currentOption];
+			Options opts = versions.getOptions(currentOption);
+			for (Option opt : opts) {
+				// Compute the next available options index
+				int next = getNextOptionIndexAvailable(currentOption + 1,
+						lastOptionChanged);
+				if (next != -1 ? _setCurrentVersion(next, lastOptionChanged,
+						options) : versions.hasVersion(options)) {
+					return true;
+				}
+				options[currentOption] = opt;
+			}
+			options[currentOption] = before;
+			return false;
+		}
+		return true;
+	}
 
-    private int getNextOptionIndexAvailable(int desiredIndex, int forbidden) {
-        int index = desiredIndex;
-        for (; index < versions.getOptionsCount() && index == forbidden; index++)
-            ;
-        return index < versions.getOptionsCount() && index != forbidden ? index : -1;
-    }
+	private int getNextOptionIndexAvailable(int desiredIndex, int forbidden) {
+		int index = desiredIndex;
+		for (; index < versions.getOptionsCount() && index == forbidden; index++)
+			;
+		return index < versions.getOptionsCount() && index != forbidden ? index
+				: -1;
+	}
 
-    public void setQuantity(int quantity) {
-        if (quantity < 1) {
-            mQuantity = 1;
-        } else {
-            mQuantity = quantity;
-        }
-    }
+	public void setQuantity(int quantity) {
+		if (quantity < 1) {
+			mQuantity = 1;
+		} else {
+			mQuantity = quantity;
+		}
+	}
 
-    public int getQuantity() {
-        return mQuantity;
-    }
+	public int getQuantity() {
+		return mQuantity;
+	}
 
-    public Version getCurrentVersion() {
-        return versions.getVersion(mCurrentVersionKey);
-    }
+	public Version getCurrentVersion() {
+		return versions.getVersion(mCurrentVersionKey);
+	}
 
-    public boolean hasVersion() {
-        return versions.getVersionsCount() > 0;
-    }
+	public boolean hasVersion() {
+		return versions.getVersionsCount() > 0;
+	}
 
-    @Override
-    public long getId() {
-        return url.hashCode();
-    }
+	@Override
+	public long getId() {
+		return url.hashCode();
+	}
 
-    @Override
-    public boolean isValid() {
-        return hasVersion() && getCurrentVersion().isValid() && merchant != null;
-    }
+	@Override
+	public boolean isValid() {
+		return hasVersion() && getCurrentVersion().isValid()
+				&& merchant != null;
+	}
 
-    public boolean isDone() {
-        return false;
-    }
+	public boolean isDone() {
+		return false;
+	}
 
 }
--- src/com/shopelia/android/model/Version.java
+++ src/com/shopelia/android/model/Version.java
@@ -14,198 +14,208 @@ import com.shopelia.android.utils.ParcelUtils;
 
 public class Version implements BaseModel<Version> {
 
-    public interface Api {
-        String ID = "id";
-        String NAME = "name";
-        String DESCRIPTION = "description";
-        String IMAGE_URL = "image_url";
-        String PRODUCT_PRICE = "price";
-        String SHIPPING_PRICE = "price_shipping";
-        String SHIPPING_EXTRAS = "shipping_info";
-        String CASHFRONT_VALUE = "cashfront_value";
-        String PRICE_STRIKEOUT = "price_strikeout";
-        String AVAILABILITY_INFO = "availability_info";
-    }
+	public interface Api {
+		String ID = "id";
+		String NAME = "name";
+		String DESCRIPTION = "description";
+		String IMAGE_URL = "image_url";
+		String PRODUCT_PRICE = "price";
+		String SHIPPING_PRICE = "price_shipping";
+		String SHIPPING_EXTRAS = "shipping_info";
+		String CASHFRONT_VALUE = "cashfront_value";
+		String PRICE_STRIKEOUT = "price_strikeout";
+		String AVAILABILITY_INFO = "availability_info";
+	}
 
-    public static final BigDecimal NO_PRICE = new BigDecimal("-1");
-    public static final long FIRST_OPTION = 0;
-    private final long id;
+	public static final BigDecimal NO_PRICE = new BigDecimal("-1");
+	public static final long FIRST_OPTION = 0;
+	private final long id;
 
-    // Prices
-    private final BigDecimal productPrice;
-    private final BigDecimal shippingPrice;
-    private final BigDecimal cashfrontValue;
-    private final BigDecimal priceStrikeOut;
+	// Prices
+	private final BigDecimal productPrice;
+	private final BigDecimal shippingPrice;
+	private final BigDecimal cashfrontValue;
+	private final BigDecimal priceStrikeOut;
 
-    // Informations
-    public final String name;
-    public final String imageUrl;
-    public final String description;
-    public final String shippingExtra;
-    public final String availabilityInfo;
+	// Informations
+	public final String name;
+	public final String imageUrl;
+	public final String description;
+	public final String shippingExtra;
+	public final String availabilityInfo;
 
-    // Options
-    private long optionsHashcode = FIRST_OPTION;
-    private Option[] options;
+	// Options
+	private long optionsHashcode = FIRST_OPTION;
+	private Option[] options;
 
-    private Version(JSONObject object) throws JSONException {
-        // Informations
-        id = object.getLong(Api.ID);
-        name = object.getString(Api.NAME);
-        description = object.optString(Api.DESCRIPTION);
-        shippingExtra = object.optString(Api.SHIPPING_EXTRAS);
-        availabilityInfo = shippingExtra == null || !shippingExtra.equalsIgnoreCase(object.optString(Api.AVAILABILITY_INFO)) ? object
-                .optString(Api.AVAILABILITY_INFO) : null;
-        imageUrl = object.optString(Api.IMAGE_URL);
+	private Version(JSONObject object) throws JSONException {
+		// Informations
+		id = object.optLong(Api.ID);
+		name = object.getString(Api.NAME);
+		description = object.optString(Api.DESCRIPTION);
+		shippingExtra = object.optString(Api.SHIPPING_EXTRAS);
+		availabilityInfo = shippingExtra == null
+				|| !shippingExtra.equalsIgnoreCase(object
+						.optString(Api.AVAILABILITY_INFO)) ? object
+				.optString(Api.AVAILABILITY_INFO) : null;
+		imageUrl = object.optString(Api.IMAGE_URL);
 
-        // Prices informations
-        productPrice = JsonUtils.optBigDecimal(object, Api.PRODUCT_PRICE, NO_PRICE).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-        shippingPrice = JsonUtils.optBigDecimal(object, Api.SHIPPING_PRICE, NO_PRICE).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-        cashfrontValue = JsonUtils.optBigDecimal(object, Api.CASHFRONT_VALUE, BigDecimal.ZERO).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-        priceStrikeOut = JsonUtils.optBigDecimal(object, Api.PRICE_STRIKEOUT, NO_PRICE).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-    }
+		// Prices informations
+		productPrice = JsonUtils.optBigDecimal(object, Api.PRODUCT_PRICE,
+				NO_PRICE).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+		shippingPrice = JsonUtils.optBigDecimal(object, Api.SHIPPING_PRICE,
+				NO_PRICE).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+		cashfrontValue = JsonUtils.optBigDecimal(object, Api.CASHFRONT_VALUE,
+				BigDecimal.ZERO).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+		priceStrikeOut = JsonUtils.optBigDecimal(object, Api.PRICE_STRIKEOUT,
+				NO_PRICE).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+	}
 
-    private Version(Parcel source) {
-        // Informations
-        id = source.readLong();
-        name = source.readString();
-        description = source.readString();
-        shippingExtra = source.readString();
-        availabilityInfo = source.readString();
-        imageUrl = source.readString();
+	private Version(Parcel source) {
+		// Informations
+		id = source.readLong();
+		name = source.readString();
+		description = source.readString();
+		shippingExtra = source.readString();
+		availabilityInfo = source.readString();
+		imageUrl = source.readString();
 
-        // Prices
-        productPrice = ParcelUtils.readBigDecimal(source, NO_PRICE);
-        shippingPrice = ParcelUtils.readBigDecimal(source, NO_PRICE);
-        cashfrontValue = ParcelUtils.readBigDecimal(source, BigDecimal.ZERO);
-        priceStrikeOut = ParcelUtils.readBigDecimal(source, NO_PRICE);
+		// Prices
+		productPrice = ParcelUtils.readBigDecimal(source, NO_PRICE);
+		shippingPrice = ParcelUtils.readBigDecimal(source, NO_PRICE);
+		cashfrontValue = ParcelUtils.readBigDecimal(source, BigDecimal.ZERO);
+		priceStrikeOut = ParcelUtils.readBigDecimal(source, NO_PRICE);
 
-        // Options
-        optionsHashcode = source.readLong();
-        Parcelable[] p = source.readParcelableArray(Option.class.getClassLoader());
-        options = new Option[p.length];
-        for (int index = 0; index < p.length; index++) {
-            options[index] = (Option) p[index];
-        }
-    }
+		// Options
+		optionsHashcode = source.readLong();
+		Parcelable[] p = source.readParcelableArray(Option.class
+				.getClassLoader());
+		options = new Option[p.length];
+		for (int index = 0; index < p.length; index++) {
+			options[index] = (Option) p[index];
+		}
+	}
 
-    public void setOptions(long optionsHash, Option[] options) {
-        optionsHash = Long.valueOf(optionsHash);
-        this.options = options;
-    }
+	public void setOptions(long optionsHash, Option[] options) {
+		optionsHash = Long.valueOf(optionsHash);
+		this.options = options;
+	}
 
-    public Option[] getOptions() {
-        return options;
-    }
+	public Option[] getOptions() {
+		return options;
+	}
 
-    public long getOptionHashcode() {
-        return optionsHashcode;
-    }
+	public long getOptionHashcode() {
+		return optionsHashcode;
+	}
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+	@Override
+	public int describeContents() {
+		return 0;
+	}
 
-    public Uri getImageUri() {
-        return Uri.parse(imageUrl);
-    }
+	public Uri getImageUri() {
+		return Uri.parse(imageUrl);
+	}
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        // Informations
-        dest.writeLong(id);
-        dest.writeString(name);
-        dest.writeString(description);
-        dest.writeString(shippingExtra);
-        dest.writeString(availabilityInfo);
-        dest.writeString(imageUrl);
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// Informations
+		dest.writeLong(id);
+		dest.writeString(name);
+		dest.writeString(description);
+		dest.writeString(shippingExtra);
+		dest.writeString(availabilityInfo);
+		dest.writeString(imageUrl);
 
-        // Prices
-        ParcelUtils.writeBigDecimal(dest, productPrice, NO_PRICE);
-        ParcelUtils.writeBigDecimal(dest, shippingPrice, NO_PRICE);
-        ParcelUtils.writeBigDecimal(dest, cashfrontValue, BigDecimal.ZERO);
-        ParcelUtils.writeBigDecimal(dest, priceStrikeOut, NO_PRICE);
+		// Prices
+		ParcelUtils.writeBigDecimal(dest, productPrice, NO_PRICE);
+		ParcelUtils.writeBigDecimal(dest, shippingPrice, NO_PRICE);
+		ParcelUtils.writeBigDecimal(dest, cashfrontValue, BigDecimal.ZERO);
+		ParcelUtils.writeBigDecimal(dest, priceStrikeOut, NO_PRICE);
 
-        // Options
-        dest.writeLong(optionsHashcode);
-        dest.writeParcelableArray(options, flags);
-    }
+		// Options
+		dest.writeLong(optionsHashcode);
+		dest.writeParcelableArray(options, flags);
+	}
 
-    public static Version inflate(JSONObject object) throws JSONException {
-        return new Version(object);
-    }
+	public static Version inflate(JSONObject object) throws JSONException {
+		return new Version(object);
+	}
 
-    @Override
-    public JSONObject toJson() throws JSONException {
-        return null;
-    }
+	@Override
+	public JSONObject toJson() throws JSONException {
+		return null;
+	}
 
-    @Override
-    public void merge(Version item) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public void merge(Version item) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public long getId() {
-        return id;
-    }
+	@Override
+	public long getId() {
+		return id;
+	}
 
-    @Override
-    public boolean isValid() {
-        return productPrice != NO_PRICE && shippingPrice != NO_PRICE;
-    }
+	@Override
+	public boolean isValid() {
+		return productPrice != NO_PRICE && shippingPrice != NO_PRICE;
+	}
 
-    public static final Parcelable.Creator<Version> CREATOR = new Creator<Version>() {
+	public static final Parcelable.Creator<Version> CREATOR = new Creator<Version>() {
 
-        @Override
-        public Version createFromParcel(Parcel source) {
-            return new Version(source);
-        }
+		@Override
+		public Version createFromParcel(Parcel source) {
+			return new Version(source);
+		}
 
-        @Override
-        public Version[] newArray(int size) {
-            return new Version[size];
-        }
+		@Override
+		public Version[] newArray(int size) {
+			return new Version[size];
+		}
 
-    };
+	};
 
-    // Price utility methods
-    public BigDecimal getTotalPrice(int quantity) {
-        return productPrice.multiply(BigDecimal.valueOf(quantity)).add(shippingPrice).subtract(cashfrontValue);
-    }
+	// Price utility methods
+	public BigDecimal getTotalPrice(int quantity) {
+		return productPrice.multiply(BigDecimal.valueOf(quantity))
+				.add(shippingPrice).subtract(cashfrontValue);
+	}
 
-    public BigDecimal getExpectedTotalPrice(int quantity) {
-        return productPrice.multiply(BigDecimal.valueOf(quantity)).add(shippingPrice.equals(NO_PRICE) ? BigDecimal.ZERO : shippingPrice);
-    }
+	public BigDecimal getExpectedTotalPrice(int quantity) {
+		return productPrice.multiply(BigDecimal.valueOf(quantity)).add(
+				shippingPrice.equals(NO_PRICE) ? BigDecimal.ZERO
+						: shippingPrice);
+	}
 
-    public boolean isShippingFree() {
-        return shippingPrice.compareTo(BigDecimal.ZERO) <= 0;
-    }
+	public boolean isShippingFree() {
+		return shippingPrice.compareTo(BigDecimal.ZERO) <= 0;
+	}
 
-    public BigDecimal getExpectedCashfrontValue(int quantity) {
-        return cashfrontValue.multiply(BigDecimal.valueOf(quantity));
-    }
+	public BigDecimal getExpectedCashfrontValue(int quantity) {
+		return cashfrontValue.multiply(BigDecimal.valueOf(quantity));
+	}
 
-    /**
-     * Raw price no cashfront
-     * 
-     * @return
-     */
-    public BigDecimal getPrice(int quantity) {
-        return productPrice.multiply(BigDecimal.valueOf(quantity));
-    }
+	/**
+	 * Raw price no cashfront
+	 * 
+	 * @return
+	 */
+	public BigDecimal getPrice(int quantity) {
+		return productPrice.multiply(BigDecimal.valueOf(quantity));
+	}
 
-    public BigDecimal getStrikeoutPrice() {
-        return priceStrikeOut;
-    }
+	public BigDecimal getStrikeoutPrice() {
+		return priceStrikeOut;
+	}
 
-    public BigDecimal getShippingPrice() {
-        return shippingPrice;
-    }
+	public BigDecimal getShippingPrice() {
+		return shippingPrice;
+	}
 
-    public boolean hasCashfront() {
-        return cashfrontValue.compareTo(BigDecimal.ZERO) > 0;
-    }
+	public boolean hasCashfront() {
+		return cashfrontValue.compareTo(BigDecimal.ZERO) > 0;
+	}
 
 }
