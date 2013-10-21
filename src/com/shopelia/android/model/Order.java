a41ee6a13a5c8b2218077bb9fb8d4594129f2ed6
Pierre Pollastri <pollastri.p@gmail.com>
2013-10-21 14:01:52 +0200

    Add Tracker into the Order model

--- src/com/shopelia/android/model/Order.java
+++ src/com/shopelia/android/model/Order.java
@@ -9,140 +9,150 @@ import com.shopelia.android.utils.ParcelUtils;
 
 public class Order implements BaseModel<Order> {
 
-    public interface Api {
-        String UUID = "uuid";
-        String USER = "user";
-        String ADDRESS = "address";
-        String PAYMENT_CARD = "payment_card";
-        String PRODUCT = "product";
-        String PRODUCTS = "products";
-        String EXPECTED_PRICE_TOTAL = "expected_price_total";
-        String EXPECTED_CASHFRONT_VALUE = "expected_cashfront_value";
-        String EXPECTED_SHIPPING_PRICE = "expected_shipping_price";
-        String EXPECTED_PRODUCT_PRICE = "expected_product_price";
-        String ORDER = "order";
-    }
+	public interface Api {
+		String UUID = "uuid";
+		String USER = "user";
+		String ADDRESS = "address";
+		String PAYMENT_CARD = "payment_card";
+		String PRODUCT = "product";
+		String PRODUCTS = "products";
+		String EXPECTED_PRICE_TOTAL = "expected_price_total";
+		String EXPECTED_CASHFRONT_VALUE = "expected_cashfront_value";
+		String EXPECTED_SHIPPING_PRICE = "expected_shipping_price";
+		String EXPECTED_PRODUCT_PRICE = "expected_product_price";
+		String ORDER = "order";
+		String TRACKER = "tracker";
+	}
 
-    public String uuid = NO_UUID;
+	public String uuid = NO_UUID;
 
-    public Product product;
+	public Product product;
 
-    // Shipping
-    public Address address;
+	// Shipping
+	public Address address;
 
-    // Payment card
-    public PaymentCard card;
+	// Payment card
+	public PaymentCard card;
 
-    // User
-    public User user;
+	// User
+	public User user;
 
-    public Order() {
+	public final String tracker;
 
-    }
+	public Order(String tracker) {
+		this.tracker = tracker;
+	}
 
-    private Order(Parcel source) {
-        uuid = source.readString();
-        product = ParcelUtils.readParcelable(source, Product.class.getClassLoader());
-        card = ParcelUtils.readParcelable(source, PaymentCard.class.getClassLoader());
-        user = ParcelUtils.readParcelable(source, User.class.getClassLoader());
-        address = ParcelUtils.readParcelable(source, Address.class.getClassLoader());
-    }
+	private Order(Parcel source) {
+		uuid = source.readString();
+		tracker = source.readString();
+		product = ParcelUtils.readParcelable(source,
+				Product.class.getClassLoader());
+		card = ParcelUtils.readParcelable(source,
+				PaymentCard.class.getClassLoader());
+		user = ParcelUtils.readParcelable(source, User.class.getClassLoader());
+		address = ParcelUtils.readParcelable(source,
+				Address.class.getClassLoader());
+	}
 
-    public void updateUser(User update) {
-        if (!update.addresses.contains(address)) {
-            address = update.getDefaultAddress();
-        }
-        if (!update.paymentCards.contains(card)) {
-            card = update.getDefaultPaymentCard();
-        }
-        user = update;
-    }
+	public void updateUser(User update) {
+		if (!update.addresses.contains(address)) {
+			address = update.getDefaultAddress();
+		}
+		if (!update.paymentCards.contains(card)) {
+			card = update.getDefaultPaymentCard();
+		}
+		user = update;
+	}
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+	@Override
+	public int describeContents() {
+		return 0;
+	}
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(uuid);
-        ParcelUtils.writeParcelable(dest, product, flags);
-        ParcelUtils.writeParcelable(dest, card, flags);
-        ParcelUtils.writeParcelable(dest, user, flags);
-        ParcelUtils.writeParcelable(dest, address, flags);
-    }
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(uuid);
+		dest.writeString(tracker);
+		ParcelUtils.writeParcelable(dest, product, flags);
+		ParcelUtils.writeParcelable(dest, card, flags);
+		ParcelUtils.writeParcelable(dest, user, flags);
+		ParcelUtils.writeParcelable(dest, address, flags);
+	}
 
-    public static final Creator<Order> CREATOR = new Creator<Order>() {
+	public static final Creator<Order> CREATOR = new Creator<Order>() {
 
-        @Override
-        public Order[] newArray(int size) {
-            return new Order[size];
-        }
+		@Override
+		public Order[] newArray(int size) {
+			return new Order[size];
+		}
 
-        @Override
-        public Order createFromParcel(Parcel source) {
-            return new Order(source);
-        }
-    };
+		@Override
+		public Order createFromParcel(Parcel source) {
+			return new Order(source);
+		}
+	};
 
-    public static Order inflate(JSONObject object) {
-        Order order = new Order();
-        order.uuid = object.optString(Api.UUID, NO_UUID);
-        try {
-            order.product = Product.inflate(object.getJSONObject(Api.PRODUCT));
-        } catch (JSONException e) {
-            // Nothing to be done
-        }
-        if (object.has(Api.ADDRESS)) {
-            try {
-                order.address = Address.inflate(object.getJSONObject(Api.ADDRESS));
-            } catch (JSONException e) {
-                // Nothing to be done
-            }
-        }
-        if (object.has(Api.PAYMENT_CARD)) {
-            try {
-                order.card = PaymentCard.inflate(object.getJSONObject(Api.PAYMENT_CARD));
-            } catch (JSONException e) {
-                // Nothing to be done
-            }
-        }
-        if (object.has(Api.USER)) {
-            try {
-                order.user = User.inflate(object.getJSONObject(Api.USER));
-            } catch (JSONException e) {
-                // Nothing to be done
-            }
-        }
-        return order;
-    }
+	public static Order inflate(JSONObject object) {
+		Order order = new Order((String) null);
+		order.uuid = object.optString(Api.UUID, NO_UUID);
+		try {
+			order.product = Product.inflate(object.getJSONObject(Api.PRODUCT));
+		} catch (JSONException e) {
+			// Nothing to be done
+		}
+		if (object.has(Api.ADDRESS)) {
+			try {
+				order.address = Address.inflate(object
+						.getJSONObject(Api.ADDRESS));
+			} catch (JSONException e) {
+				// Nothing to be done
+			}
+		}
+		if (object.has(Api.PAYMENT_CARD)) {
+			try {
+				order.card = PaymentCard.inflate(object
+						.getJSONObject(Api.PAYMENT_CARD));
+			} catch (JSONException e) {
+				// Nothing to be done
+			}
+		}
+		if (object.has(Api.USER)) {
+			try {
+				order.user = User.inflate(object.getJSONObject(Api.USER));
+			} catch (JSONException e) {
+				// Nothing to be done
+			}
+		}
+		return order;
+	}
 
-    @Override
-    public JSONObject toJson() throws JSONException {
-        JSONObject object = new JSONObject();
-        object.put(Api.UUID, uuid);
-        object.put(Api.ADDRESS, address != null ? address.toJson() : null);
-        object.put(Api.USER, user != null ? user.toJson() : null);
-        object.put(Api.PRODUCT, product != null ? product.toJson() : null);
-        object.put(Api.PAYMENT_CARD, card != null ? card.toJson() : null);
-        return object;
-    }
+	@Override
+	public JSONObject toJson() throws JSONException {
+		JSONObject object = new JSONObject();
+		object.put(Api.UUID, uuid);
+		object.put(Api.ADDRESS, address != null ? address.toJson() : null);
+		object.put(Api.USER, user != null ? user.toJson() : null);
+		object.put(Api.PRODUCT, product != null ? product.toJson() : null);
+		object.put(Api.PAYMENT_CARD, card != null ? card.toJson() : null);
+		object.put(Api.TRACKER, tracker);
+		return object;
+	}
 
-    @Override
-    public void merge(Order item) {
-        // TODO Auto-generated method stub
+	@Override
+	public void merge(Order item) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public long getId() {
-        return uuid != NO_UUID ? uuid.hashCode() : NO_ID;
-    }
+	@Override
+	public long getId() {
+		return uuid != NO_UUID ? uuid.hashCode() : NO_ID;
+	}
 
-    @Override
-    public boolean isValid() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean isValid() {
+		return false;
+	}
 
 }
