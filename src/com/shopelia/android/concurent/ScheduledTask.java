c075b5e02b6bc656488f5376d2299728d7b483a9
Pierre Pollastri <pollastri.p@gmail.com>
2013-11-08 14:25:07 +0100

    Add colors

--- src/com/shopelia/android/concurent/ScheduledTask.java
+++ src/com/shopelia/android/concurent/ScheduledTask.java
@@ -13,72 +13,75 @@ import android.os.Handler;
  */
 public class ScheduledTask {
 
-    private static final long SINGLE_SHOT = -1L;
+	private static final long SINGLE_SHOT = -1L;
 
-    private Handler mHandler;
-    private long mPeriod = SINGLE_SHOT;
-    private long mDelay;
-    private Runnable mRunnable;
+	private Handler mHandler;
+	private long mPeriod = SINGLE_SHOT;
+	private long mDelay;
+	private Runnable mRunnable;
 
-    private Object mToken = new Object();
+	private Object mToken = new Object();
 
-    public ScheduledTask() {
-        this(new Handler());
-    }
+	public ScheduledTask() {
+		this(new Handler());
+	}
 
-    /**
-     * Use a custom handler
-     * 
-     * @param handler
-     */
-    public ScheduledTask(Handler handler) {
-        mHandler = handler;
-    }
+	/**
+	 * Use a custom handler
+	 * 
+	 * @param handler
+	 */
+	public ScheduledTask(Handler handler) {
+		mHandler = handler;
+	}
 
-    public void scheduleAtFixedRate(final Runnable runnable, final long delay, final long period) {
-        mRunnable = runnable;
-        mPeriod = period;
-        mDelay = delay;
-        start();
-    }
+	public void scheduleAtFixedRate(final Runnable runnable, final long delay,
+			final long period) {
+		mRunnable = runnable;
+		mPeriod = period;
+		mDelay = delay;
+		start();
+	}
 
-    public void schedule(final Runnable runnable, final long delay) {
-        mRunnable = runnable;
-        mPeriod = SINGLE_SHOT;
-        mDelay = delay;
-        start();
-    }
+	public void schedule(final Runnable runnable, final long delay) {
+		mRunnable = runnable;
+		mPeriod = SINGLE_SHOT;
+		mDelay = delay;
+		start();
+	}
 
-    public void stop() {
-        if (mHandler != null) {
-            mPeriod = SINGLE_SHOT;
-            mHandler.removeCallbacksAndMessages(mToken);
-        }
-    }
+	public void stop() {
+		if (mHandler != null) {
+			mPeriod = SINGLE_SHOT;
+			mRunnable = null;
+			mHandler.removeCallbacksAndMessages(mToken);
+		}
+	}
 
-    public void cancel() {
-        stop();
-    }
+	public void cancel() {
+		stop();
+	}
 
-    private void start() {
-        if (mHandler == null) {
-            mHandler = new Handler();
-        }
-        mHandler.postDelayed(mPrivateRunnable, mDelay + (mPeriod != SINGLE_SHOT ? mPeriod : 0));
-    }
+	private void start() {
+		if (mHandler == null) {
+			mHandler = new Handler();
+		}
+		mHandler.postDelayed(mPrivateRunnable, mDelay
+				+ (mPeriod != SINGLE_SHOT ? mPeriod : 0));
+	}
 
-    private Runnable mPrivateRunnable = new Runnable() {
+	private Runnable mPrivateRunnable = new Runnable() {
 
-        @Override
-        public void run() {
-            mDelay = 0L;
-            if (mRunnable != null) {
-                mRunnable.run();
-                if (mPeriod != SINGLE_SHOT) {
-                    mHandler.postDelayed(mPrivateRunnable, mPeriod);
-                }
-            }
-        }
-    };
+		@Override
+		public void run() {
+			mDelay = 0L;
+			if (mRunnable != null) {
+				mRunnable.run();
+				if (mPeriod != SINGLE_SHOT) {
+					mHandler.postDelayed(mPrivateRunnable, mPeriod);
+				}
+			}
+		}
+	};
 
 }
